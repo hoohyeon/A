a33f0699c0d17041916a8257f4a89eacdbe8fd5a
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-04-02 14:07:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 136 ms, Memory: 41524 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -7,12 +7,14 @@ dp[0][0] = tri[0][0]
 for i in range(1, n):
     for j in range(i+1):
         if j == 0:
-            dp[i][j] = dp[i-1][0] + tri[i][0]
+            prev = dp[i-1][0]
 
         elif j == i:
-            dp[i][j] = dp[i-1][j-1] + tri[i][j]
+            prev = dp[i-1][j-1]
 
         else:
-            dp[i][j] = max(dp[i-1][j-1] + tri[i][j], dp[i-1][j] + tri[i][j])
+            prev = max(dp[i-1][j-1], dp[i-1][j])
+
+        dp[i][j] = prev + tri[i][j]
 
 print(max(dp[n-1]))
