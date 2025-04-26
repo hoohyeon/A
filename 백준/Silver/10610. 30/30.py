1dc7cb9e7d7ad642e91193e53f859970bccbbd00
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-04-26 19:16:33 +0900

    [Silver IV] Title: 30, Time: 64 ms, Memory: 33976 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,16 @@
+n = list(input())
+n = sorted(n, reverse=True)
+
+if n[-1] != '0':
+    print(-1)
+    
+else:
+    sum = 0
+    for i in n:
+        sum += int(i)
+        
+    if sum % 3 != 0:
+        print(-1)
+        
+    else:
+        print("".join(n))
