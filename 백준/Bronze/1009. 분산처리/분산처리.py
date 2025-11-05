900811a921b269ea77c82d52424a9c9ecf13f5cd
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-11-06 00:29:34 +0900

    [Bronze II] Title: 분산처리, Time: 4200 ms, Memory: 32912 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())
+    
+    a = a % 10
+    
+    if a == 0:
+        print(10)
+        
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+        
+    elif a == 4 or a == 9:
+        if b % 2 == 0:
+            print((a**2) % 10)
+        else:
+            print(a)
+    
+    else:
+        print((a**b) % 10)
