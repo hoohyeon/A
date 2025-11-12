acb2c5e8010ea5b39026a955c2e690a5bdf7a6e5
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-11-12 22:17:11 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,18 @@
+n = int(input())
+height = list(map(int, input().split()))
+result = [0] * n
+
+for i in range(n):
+
+    cnt = 0
+    
+    for j in range(n):
+
+        if (height[i] == cnt) and (result[j] == 0):
+            result[j] = i+1
+            break
+
+        elif result[j] == 0:
+            cnt += 1
+
+print(*result)
