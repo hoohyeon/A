6304cc4d8c53a13ec6e5517bd82dbcaf8a722687
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-11-12 22:20:00 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,15 +1,3 @@
 n = int(input())
 
-stick = 64
-cnt = 0
-
-while n > 0:
-    if n >= stick:
-        n -= stick
-        cnt += 1
-
-    else:
-        stick //= 2
-
-
-print(cnt)
+print(sum(list(map(int, bin(n)[2:]))))
