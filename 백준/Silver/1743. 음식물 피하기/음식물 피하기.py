1e0c941c0af86163bd339e8fa1b2295fb123cbd6
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-11-16 00:01:58 +0900

    [Silver I] Title: 음식물 피하기, Time: 164 ms, Memory: 34296 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,4 +1,5 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10 ** 6)
 
 n, m, k = map(int, input().split())
 grid = [['.'] * m for _ in range(n)]
@@ -9,37 +10,34 @@ for _ in range(k):
     
 visit = [[False] * m for _ in range(n)]
 
-def bfs(sr, sc):
-    q = deque([(sr, sc)])
-    visit[sr][sc] = 1
+def dfs(sr, sc):
+
+    visit[sr][sc] = True
     cnt = 1
-    
-    while q:
-        r, c = q.popleft()
+            
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nr, nc = sr + dr, sc + dc
         
-        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nr, nc = r + dr, c + dc
+        if not (0 <= nr < n and 0 <= nc < m):
+            continue
+        
+        if not grid[nr][nc] == '#':
+            continue
             
-            if not (0 <= nr < n and 0 <= nc < m):
-                continue
-            
-            if not grid[nr][nc] == '#':
-                continue
-                
-            if visit[nr][nc]:
-                continue
-            
-            visit[nr][nc] = True
-            q.append((nr, nc))
-            cnt += 1
+        if visit[nr][nc]:
+            continue
+        
+        visit[nr][nc] = True
+        cnt += dfs(nr, nc)
             
     return cnt
 
 max_cnt = 0
 for i in range(n):
     for j in range(m):
-        if grid[i][j] == '.':
+        if grid[i][j] == '.' or visit[i][j]:
             continue
-        max_cnt = max(max_cnt, bfs(i, j))
         
-print(max_cnt)    
+        max_cnt = max(max_cnt, dfs(i, j))
+
+print(max_cnt)
