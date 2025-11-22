fb6fbbeaa81ffb638e93c9f6c31bf4302bebfe8d
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-11-22 23:10:17 +0900

    [Bronze I] Title: 약수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -2,14 +2,4 @@ n = int(input())
 
 num_list = list(map(int, input().split()))
 
-num_list.sort()
-
-l = len(num_list)
-
-if l == 1:
-    ans = num_list[0] ** 2
-    
-else:
-    ans = num_list[0] * num_list[-1]
-    
-print(ans)
+print(max(num_list) * min(num_list))
