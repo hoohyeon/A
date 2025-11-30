2eee9a83351181662ba3fe0b199d207ce809d8fe
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-01 02:28:38 +0900

    [Silver II] Title: 타일링, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,14 @@
+dp = [0] * (251)
+dp[0], dp[1] = 1, 1
+    
+while True:
+    try:
+        n = int(input())
+        
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + 2 * dp[i-2]
+        
+        print(dp[n])
+      
+    except:
+        break
