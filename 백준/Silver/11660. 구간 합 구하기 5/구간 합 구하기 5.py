a8c18641e8ec8428ac68e81f3a5f98fff201cc68
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-04 21:40:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1064 ms, Memory: 106832 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+pre = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        pre[i][j] = pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1] + grid[i-1][j-1]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    res = pre[x2][y2] - pre[x1-1][y2] - pre[x2][y1-1] + pre[x1-1][y1-1]
+
+    print(res)
