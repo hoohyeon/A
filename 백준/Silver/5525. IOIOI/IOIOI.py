2f0de29991e9ec616e54189146f57b521a9720c0
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-07 17:16:25 +0900

    [Silver I] Title: IOIOI, Time: 332 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,18 @@
+n = int(input())
+m = int(input())
+s = input()
+
+cnt, ans, i = 0, 0, 0
+while i < m-1:
+    if s[i:i+3] == 'IOI':
+        cnt += 1
+        i += 2
+        
+        if cnt == n:
+            ans += 1
+            cnt -= 1            
+    else:
+        cnt = 0
+        i += 1
+        
+print(ans)
