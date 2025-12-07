bd86bff560738bbd53f01ece4aa1a7f51f4370b0
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-07 15:58:45 +0900

    [Silver II] Title: 마인크래프트, Time: 148 ms, Memory: 33736 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,41 +1,44 @@
-import sys
-input = sys.stdin.readline
-
 n, m, b = map(int, input().split())
-
 ground = [list(map(int, input().split())) for _ in range(n)]
 
-min_h = min(map(min, ground))
-max_h = max(map(max, ground))
+h_count = [0] * 257
+for row in range(n):
+    for col in range(m):
+        height = ground[row][col]
+        h_count[height] += 1
+
+min_h = min(i for i in range(257) if h_count[i] > 0)
+max_h = max(i for i in range(257) if h_count[i] > 0)
 
 best_time, best_height = int(1e9), 0 
-for h in range(min_h, max_h+1):
+for target_h in range(min_h, max_h+1):
     time, inventory = 0, b
+
+    for cur_h in range(min_h, max_h+1):
+        cnt = h_count[cur_h]
+        
+        if cnt == 0:
+            continue
     
-    for row in range(n):
-        for col in range(m):
-            cur = ground[row][col]
+        # 블록 제거
+        if cur_h > target_h:
+            diff = cur_h - target_h
+            time += diff * 2 * cnt
+            inventory += diff * cnt
             
-            # 블록 제거
-            if cur > h:
-                diff = cur - h
-                time += diff * 2
-                inventory += diff
-            
-            # 블록 놓기
-            elif cur < h:
-                diff = h - cur
-                time += diff
-                inventory -= diff
-                
+        elif cur_h < target_h:
+            diff = target_h - cur_h
+            time += diff * cnt
+            inventory -= diff * cnt
+        
     if inventory < 0:
         continue
     
     if time < best_time:
         best_time = time
-        best_height = h
+        best_height = target_h
     
-    elif time == best_time and h > best_height:
-        best_height = h
-        
+    elif time == best_time and target_h > best_height:
+        best_height = target_h
+            
 print(best_time, best_height)
