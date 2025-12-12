b8620cd303d829465306be03c3a22afe0d0572f0
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-12 23:38:57 +0900

    [Silver I] Title: 스티커, Time: 664 ms, Memory: 49964 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    score = [list(map(int, input().split())) for _ in range(2)]
+
+    dp = [[0] * (n+1) for _ in range(2)]
+    dp[0][1] = score[0][0]
+    dp[1][1] = score[1][0]
+    
+    for i in range(2, n+1):
+        dp[0][i] = max(dp[1][i-2], dp[1][i-1]) + score[0][i-1]
+        dp[1][i] = max(dp[0][i-2], dp[0][i-1]) + score[1][i-1]
+
+    print(max(dp[0][n], dp[1][n]))
