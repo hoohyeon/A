0da5dd7088f8d99c7e185086577596a88056535e
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-21 16:46:13 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())    
+    coins = list(map(int, input().split())) 
+    m = int(input())
+    
+    # dp[x] = x원 경우의 수
+    dp = [0] * (m+1)
+    dp[0] = 1
+    
+    for c in coins:
+        for money in range(c, m+1):
+            # c원짜리 동전 사용
+            dp[money] += dp[money - c]
+    
+    print(dp[m])   
