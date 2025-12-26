94530f72a1d922099caa5cb1b4e83f7905e8550d
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2025-12-26 23:40:22 +0900

    [Silver III] Title: 크로스 컨트리, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,43 +1,38 @@
-from collections import defaultdict, Counter
-
 t = int(input())
 
 for _ in range(t):
-    
     n = int(input())
-    team_number = list(map(int, input().split()))
+    team_num = list(map(int, input().split()))
     
-    count = Counter(team_number)
-    teams = defaultdict(list)
-    
-    # 완료 선수가 6명이상
+    team_count = {}
+    for team in team_num:
+        if team in team_count:
+            team_count[team] += 1
+        else:
+            team_count[team] = 1
+            
+    team_rank = {}
     rank = 1
-    for team in team_number:
-        if count[team] >= 6:
-            teams[team].append(rank)
+    for team in team_num:
+        if team_count[team] >= 6:
+            if team not in team_rank:
+                team_rank[team] = []
+            team_rank[team].append(rank)
             rank += 1
-        
+    
     winner = None
     best_score = float('inf')
     best_fifth = float('inf')
     
-    for team, order in teams.items():
-        if len(order) < 6:
-            continue
+    for team, ranks in team_rank.items():
+        score = sum(ranks[:4])
+        fifth = ranks[4]
         
-        # 상위 4명 점수 합
-        score = sum(order[:4])
-        
-        # 5번째 선수 순위
-        fifth = order[4]
-        
-        # 우승 조건
         if score < best_score:
             best_score = score
             best_fifth = fifth
             winner = team
             
-        # 동일 점수
         elif score == best_score:
             if fifth < best_fifth:
                 best_fifth = fifth
