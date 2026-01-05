e4a8aa52afbbfe9dc7dd2219cd3e5edfd5182004
hoohyeon <156387278+hoohyeon@users.noreply.github.com>
2026-01-05 22:34:05 +0900

    [Gold IV] Title: 최단경로, Time: 572 ms, Memory: 69300 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,38 +1,43 @@
-import heapq
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
 n, e = map(int, input().split())
 k = int(input())
 
-graph = [[] * (n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 for _ in range(e):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
 
 INF = int(1e9)
-distance = [INF] * (n+1)
-
 def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
+    dist = [INF] * (n+1)
+    dist[start] = 0
     
-    distance[start] = 0
+    pq = []
+    heapq.heappush(pq, (0, start))
 
-    while q:
-        dist, now = heapq.heappop(q)
+    while pq:
+        cur_cost, cur = heapq.heappop(pq)
 
-        if distance[now] < dist:
+        # 이미 더 가까운 경로
+        if cur_cost > dist[cur]:
             continue
 
-        for next, cost in graph[now]:
-            if dist + cost < distance[next]:
-                distance[next] = dist + cost
-                heapq.heappush(q, (dist + cost, next))
+        for next_node, weight in graph[cur]:
+            next_cost = cur_cost + weight
 
-dijkstra(k)
+            # next_node 사용
+            if dist[next_node] > next_cost:
+                dist[next_node] = next_cost
+                heapq.heappush(pq, (next_cost, next_node))
 
-for d in distance[1:]:
+    return dist
+
+# k를 시작점으로 하는 거리 배열
+dist = dijkstra(k)
+
+for d in dist[1:]:
     if d == INF:
         print('INF')
     else:
